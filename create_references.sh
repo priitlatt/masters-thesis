d40c37d42d67a95c1c031364522f27d9c940bfa1
Priit Lätt <lattpriit@gmail.com>
2015-02-14 19:09:30 +0200

    Forgot to add files

---
+++ create_references.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REFERENCES=references.bib
+LITERATURE_DIR=literature
+
+append_bib () {
+	echo $1
+	cat $1 >> references.bib
+	echo >> references.bib
+}
+
+touch $REFERENCES
+rm $REFERENCES
+touch $REFERENCES
+
+export -f append_bib
+find $LITERATURE_DIR -name "*.bib" -exec bash -c 'append_bib "{}"' \;
+

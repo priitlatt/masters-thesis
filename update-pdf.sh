74c4ba5ccdb0304ffe4b60a1df6a2d6276edf308
Priit Lätt <lattpriit@gmail.com>
2015-02-14 16:01:24 +0200

    Add script to update pdf on gh-pages branch

---
+++ update-pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+git checkout gh-pages
+git checkout $BRANCH -- '*.pdf'
+git commit -m "Update thesis pdf"
+git push origin gh-pages
+git checkout $BRANCH
+

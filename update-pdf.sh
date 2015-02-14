1afcb8c9227b783f53573066167105d92045f82e
Priit Lätt <lattpriit@gmail.com>
2015-02-14 16:03:58 +0200

    Update script

--- update-pdf.sh
+++ update-pdf.sh
@@ -3,6 +3,7 @@
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 git checkout gh-pages
+git pull origin gh-pages
 git checkout $BRANCH -- '*.pdf'
 git commit -m "Update thesis pdf"
 git push origin gh-pages
